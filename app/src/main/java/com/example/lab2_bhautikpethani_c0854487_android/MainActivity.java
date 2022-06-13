da3e4ec211dde6711b244239e01f93273ea31148
BhautikPethani <c0854487@mylambton.ca>
2022-06-13 17:34:21 -0400

    View binding to the main activity and dbHelper have been initialized.

--- app/src/main/java/com/example/lab2_bhautikpethani_c0854487_android/MainActivity.java
+++ app/src/main/java/com/example/lab2_bhautikpethani_c0854487_android/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.lab2_bhautikpethani_c0854487_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+
+import com.example.lab2_bhautikpethani_c0854487_android.databinding.ActivityMainBinding;
+import com.example.lab2_bhautikpethani_c0854487_android.service.DatabaseHelper;
 
 public class MainActivity extends AppCompatActivity {
+
+    DatabaseHelper dbHelper;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(LayoutInflater.from(this));
+        setContentView(binding.getRoot());
+
+        dbHelper = new DatabaseHelper(this);
     }
 }
